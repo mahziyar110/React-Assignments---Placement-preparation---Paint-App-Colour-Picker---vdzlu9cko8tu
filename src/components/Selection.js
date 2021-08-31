766809cde42f6e392a98df3d2d77bb82de1c6688
mahziyar110 <mahziyar110@gmail.com>
2021-08-31 21:37:20 +0530

    new commit

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,18 @@
+import React, { useState } from "react";
 
+function Selection({ applyColor }) {
+  const [style, setStyle] = useState({ background: "" });
+  return (
+    <div
+      className="fix-box"
+      style={style}
+      onClick={() => {
+        applyColor(setStyle);
+      }}
+    >
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+}
+
+export default Selection;
